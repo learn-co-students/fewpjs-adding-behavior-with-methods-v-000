218b60684e91a5d171683c94585beb5464bc6d44
judy <jjeanius@verizon.net>
2019-12-12 13:53:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {  //accept the parameters name and sex and will store these properties
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() { //create method speak
+        return `${this.name} says meow!`   // returns "name says meow!"
+    }
+};
+
+class Dog {
+    constructor(name, sex) { //accept the parameters name and sex and will store these properties
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {  // cerate method speak
+        return `${this.name} says woof!`  // returns name says woof!
+    }
+};
+
+
+class Bird {
+    constructor(name, sex){ //accept the parameters name and sex and will store these properties
+        this.name = name;
+        this.sex = sex;
+    }
+        speak() {  // create method speak
+            if (this.sex == "male") {
+                return `It's me! ${this.name}, the parrot!`
+        } else {
+                return `${this.name} says squawk!`
+        }
+    }
+};
