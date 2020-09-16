ead7505192dc6548565382aafbab0034555bdbb8
Pat  Pignataro <pat6284@msn.com>
2020-09-16 11:52:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+class Cat {
+  constructor(name, type){
+    this.name = name
+    this.type = type
+  }
+
+  speak(){
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog {
+  constructor(name, type){
+    this.name = name
+    this.type = type
+  }
+
+  speak(){
+    return `${this.name} says woof!`
+  }
+}
+
+class Bird {
+  constructor(name,type){
+    this.name = name
+    this.type = type
+  }
+
+  speak(){
+    return this.type == "male"? `It's me! ${this.name}, the parrot!` :
+    `${this.name} says squawk!`
+  }
+}
